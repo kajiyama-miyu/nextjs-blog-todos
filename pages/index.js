66839c6ce3f248c1e1bb22f9112807aa78e5a69f
kajiyama-miyu <miyu.kajiyama@rakus-partners.co.jp>
2021-04-23 14:38:57 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+import Auth from "../components/Auth";
+
+export default function Home() {
+  return (
+    <Layout title="Login">
+      <Auth />
+    </Layout>
+  );
+}
